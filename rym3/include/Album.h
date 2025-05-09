c4031051477a4b046d4ef58b19f2ac3407d0a048
darius-04 <108690682+darius-04@users.noreply.github.com>
2025-05-09 14:29:23 +0300

    Update Album.h

--- rym3/include/Album.h
+++ rym3/include/Album.h
@@ -10,13 +10,15 @@ class Album : public ObiectMuzical {
 private:
     std::string titlu;
     std::vector<std::shared_ptr<Melodie>> melodii;
+    int anLansare;
 
 public:
-    Album(const std::string& titlu);
+    Album(const std::string& titlu, int anLansare);
     void adaugaMelodie(std::shared_ptr<Melodie> melodie);
     void afiseazaDetalii() const override;
     std::shared_ptr<ObiectMuzical> clone() const override;
     std::string getTitlu() const;
+    int getAnLansare() const;
     const std::vector<std::shared_ptr<Melodie>>& getMelodii() const;
 };
 
